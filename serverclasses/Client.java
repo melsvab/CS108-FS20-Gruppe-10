09612273966ab37558e978368370e94f7e58934b
Dennis Grunenberg <dennis.grunenberg@stud.unibas.ch>
2020-02-28 13:27:28 +0100

    added folder serverclasses for testing reasons: server-client-communication testing.

---
+++ serverclasses/Client.java
@@ -0,0 +1,54 @@
+package serverclasses;
+
+import java.net.*;
+import java.io.*;
+
+class Client {
+
+    public static void main(String[] args) {
+
+        try {
+
+            System.out.println("Stelle Verbindung her");
+
+            Socket sock = new Socket(args[0], Integer.parseInt(args[1]));
+
+            InputStream in = sock.getInputStream();
+            OutputStream out = sock.getOutputStream();
+
+            //creat server reading thread
+            InThread th = new InThread(in);
+            Thread iT = new Thread(th);
+            iT.start();
+
+            //stream input
+            BufferedReader conin = new BufferedReader(new InputStreamReader(System.in));
+
+            String line = " ";
+
+            while (true) {
+                //reading input stream
+                line = conin.readLine();
+                if (line.equalsIgnoreCase("QUIT")) {
+                    break;
+                }
+
+                //writing to ECHO server
+                out.write(line.getBytes());
+                out.write('\n');
+            } //terminate program
+
+            System.out.println("terminating...");
+
+            in.close();
+            out.close();
+            sock.close();
+
+        } catch (IOException e) {
+            System.err.println(e.toString());
+            System.exit(1);
+        }
+
+    }
+
+}
---
+++ serverclasses/Server.java
@@ -0,0 +1,43 @@
+package serverclasses;
+
+import java.net.*;
+import java.io.*;
+
+class Server {
+
+    static final int port = 1111;
+
+    public static void main(String[] args) {
+
+        try {
+
+            System.out.println("Warte auf Port " + port + "...");
+
+            ServerSocket echo = new ServerSocket(port);
+
+            Socket socket = echo.accept();
+
+            System.out.println("Connected");
+
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
+
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write((char) c);
+                System.out.print((char) c);
+            }
+
+            System.out.println("Connection terminated");
+            socket.close();
+            echo.close();
+
+        } catch (IOException e) {
+            System.err.println(e.toString());
+            System.exit(1);
+        }
+
+    }
+
+}
