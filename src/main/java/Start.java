26659bd165d0e4139a3cee724a4d5ed3af587717
Rohail <r.gondal@stud.unibas.ch>
2020-03-28 19:07:25 +0100

    deleted Client boolean from main Class Start

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,15 +1,13 @@
 import server.Server;
 import server.Client;
 public class Start {
-    private static boolean client = true;
 
     public static void main(String[] args) {
         if (args[0].equals("server")) {
-            client = false;
             Server server = new Server(Integer.parseInt(args[1]));
             Thread serverThread = new Thread(server);
             serverThread.start();
-        }else {
+        } else if (args[0].equals("client")) {
             Client client = new Client(args[1],Integer.parseInt(args[2]));
             Thread clientThread = new Thread(client);
             clientThread.start();
